14b310d205a969804d00fa4ff02fc3b1a3ffc823
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-07-22 23:20:46 +0530

    python snippet

---
+++ basic.py
@@ -0,0 +1,20 @@
+name = "Abhinav"
+age = 20
+is_cloud_ready = True
+
+print(name)
+print(age)
+print(is_cloud_ready)
+
+print(type(age))  # <class 'int'>
+
+name = input("Enter your name: ")
+print("Welcome,", name)
+
+a = 5
+b = 2
+
+print(a + b)  # 7
+print(a ** b) # 25
+print(a // b) # 2
+print(a % b)  # 1
