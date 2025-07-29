25df59db9da2bfcc31789cd562878ad56d0660ea
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-07-29 22:06:28 +0530

    Add files via upload

---
+++ Basictasks.py
@@ -0,0 +1,31 @@
+tasks = []
+
+for i in range(1,6):
+    task = input(f"Enter task {i}: ")
+    tasks.append(task)
+
+print(f"Your tasks :")
+for t in tasks :
+    print("-",t)
+
+remove_task = ""
+
+while remove_task != "stop":
+
+    remove_task = input("Enter one task you want to remove: ")
+
+    if remove_task == "stop":
+        break
+
+    if remove_task in tasks:
+      tasks.remove(remove_task)
+      print(f"\n {remove_task} has been removed.")
+
+    else:
+        print(f"\n'{remove_task}' was not found in your list.")
+
+
+
+print("\nUpdated Task List: ")
+for t in tasks: 
+    print("-", t)
