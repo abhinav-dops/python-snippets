9522ec0835773543295f7c8e64a2fea5037fa153
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-07-31 00:12:47 +0530

    Add files via upload

---
+++ Ifel.py
@@ -0,0 +1,16 @@
+age = 18
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You are a child.")
+elif age >= 13 and age <= 19:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
