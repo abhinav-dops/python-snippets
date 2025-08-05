016556e083d3a2a83a9df46d602e79f95da56565
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-05 12:04:06 +0530

    Add files via upload

---
+++ setmini.py
@@ -0,0 +1,13 @@
+numbers = {1, 2, 3, 3, 4, 5}
+print("Original set:", numbers)  # Duplicate 3 will be removed
+
+numbers.add(10)
+print("After adding 10:", numbers)
+
+more_numbers = {3, 4, 10, 20}
+common = numbers.intersection(more_numbers)
+
+print("Common elements:", common)
+
+
+# List ([])	Tuple (())	Set ({})
