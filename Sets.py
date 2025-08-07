7d6659a07837549e22b4e1bd1dfbf9aa6a38484b
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-08 00:02:07 +0530

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -3,7 +3,7 @@ set=  {1, 2, 3, 4, 5}
 a = {1, 2, 3}
 b = {2, 3, 4}
 s = {}
-hahah
+hah
 print(a.union(b))       # {1, 2, 3, 4}
 print(a.intersection(b))# {2, 3}
 print(a.difference(b))  # {1}
@@ -21,4 +21,5 @@ s.remove("green")   # Removes item; ❌ error if not found
 s.discard("black")  # Removes item; ✅ no error if not found
 s.pop()             # Removes a random item
 s.clear()           # Empties the set
+
 
