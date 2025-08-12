ace44113f9aaab495be1939c6cca0da45360fbe5
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-12 19:54:48 +0530

    Update Tuple.py

--- Tuple.py
+++ Tuple.py
@@ -1,7 +1,7 @@
 colors = ("red", "green", "blue")
 
 single = ("apple",)   # ✅ Tuple
-not_a_tuple = ("apple")  # ❌ Just a string
+not_a_tuple = ("apple")  # Just a string
 
 print(colors[0])      # red
 print(colors[-1])     # blue
@@ -14,4 +14,5 @@ colors[1] = "yellow"   # Error: 'tuple' object does not support item assignment
 nums = (1, 2, 2, 3, 4)
 print(nums.count(2))     # Output: 2
 print(nums.index(3))     # Output: 3
+
 
