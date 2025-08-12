294ba54854583c933fb8486747caa6068a6505b1
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-12 19:54:16 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,16 @@
+colors = ("red", "green", "blue")
+
+single = ("apple",)   # ✅ Tuple
+not_a_tuple = ("apple")  # ❌ Just a string
+
+print(colors[0])      # red
+print(colors[-1])     # blue
+
+for color in colors:
+    print(color)
+
+colors[1] = "yellow"   # ❌ Error: 'tuple' object does not support item assignment
+
+nums = (1, 2, 2, 3, 4)
+print(nums.count(2))     # Output: 2
+print(nums.index(3))     # Output: 3
