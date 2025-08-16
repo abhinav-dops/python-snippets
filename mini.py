5eb99f2e28b49967dff7ed1542b2eff148662991
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-16 19:53:59 +0530

    Add files via upload

---
+++ mini.py
@@ -0,0 +1,32 @@
+profile = {}
+
+name = input("Enter your name: ")
+age = int(input("Enter your Age: "))
+favorite_language = input("Enter your favorite language: ")
+
+profile["name"] = name
+profile["age"] = age
+profile["favorite_language"] = favorite_language
+
+print("Your Profile: ")
+for key ,value in profile.items():
+    print(key, ":" , value)
+
+
+
+#nextminiproject 
+
+student = {
+    "name": "Abhinav",
+    "age": 20,
+    "course": "Python"
+}
+
+print(student)
+print(student["name"])
+
+student["age"] = 21
+student["college"] = "Xyz"
+
+for key, value in student.items():
+    print(key, ":" , value)
