61f19331c27aaedf35d4f4eb11f21b89e8dbbb7e
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-19 00:13:20 +0530

    Add files via upload

---
+++ mini2.py
@@ -0,0 +1,25 @@
+n = int(input("Enter number of profiles: "))
+
+profiles = []
+
+for i in range(n):
+
+    profile = {}
+   
+    name = input("Enter your name: ")
+    age = int(input("Enter your Age: "))
+    favorite_language = input("Enter your favorite language: ")
+
+    profile["name"] = name
+    profile["age"] = age
+    profile["favorite_language"] = favorite_language
+
+    profiles.append(profile)
+
+print("Your Profiles: ")
+for i in range(len(profiles)):
+    print("Profile", i+1, ":")
+
+    for key, value in profile[i].items():
+        print(f"{key} : {value}")
+
