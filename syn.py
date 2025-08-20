21da0642121296661083582bf9dff33d697e937b
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-20 12:42:18 +0530

    Add files via upload

---
+++ syn.py
@@ -0,0 +1,35 @@
+my_dict = {
+    "name" : "Abhinav",
+    "age" : 20,
+    "language" : "python",
+    'home' : 'india'
+}
+
+my_dict["city"] = 'delhi'
+my_dict["age"] = 25
+
+print(my_dict["age"])
+print(my_dict)
+
+print(my_dict.get("college", "Not found"))  # ✅ Safe
+
+for key in my_dict:
+    print(key)
+
+for key, value in my_dict.items():
+    print(key, ":", value)
+
+for value in my_dict.items():
+    print(value)
+
+
+
+#Method	What it does
+#dict.keys()	Returns all keys
+#dict.values()	Returns all values
+#dict.items()	Returns key-value pairs
+#dict.get(k)	Safely gets a value by key
+#dict.pop(k)	Removes a key
+#k in dict	Checks if key exists
+
+print(my_dict.get("name"))
