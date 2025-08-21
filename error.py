1625a80f0a5cef192335cee8b50d7110493e1c3e
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-21 21:47:32 +0530

    Add files via upload

---
+++ error.py
@@ -0,0 +1,11 @@
+try:
+    num = int(input("Enter a number: "))
+    result = 10/num
+    print("Result: " , result)
+
+except ZeroDivisionError :
+    print("you can't divide by zero!")
+except ValueError :
+    print("please enter a valid number!")
+finally:
+    print("Done.")
