4b50ffd2d980efce97e034e9a59c06b79408fa87
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-24 20:21:17 +0530

    Add files via upload

---
+++ JsonEH.py
@@ -0,0 +1,31 @@
+import json
+
+data = {
+    "name" : "Abhinav",
+    "age" : 20,
+    "Language" : "python"
+}
+
+with open ("data.json", "w") as file:
+    json.dump(data,file)
+
+print("data save to data.json")
+
+with open ("data.json" ,"r" )as file:
+    loaded = json.load(file)
+    print("Loaded JSON as dictionary:")
+    print(loaded)
+
+
+
+#converting json string to python dictionary
+
+import json
+
+json_string = '{"name": "Abhinav", "age": 20, "language": "Python"}'
+print("Type : ", type(json_string))
+data = json.loads(json_string)
+print("Type : ", type(data))
+print("data: " , data)
+print(data["name"])
+
