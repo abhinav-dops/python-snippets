297373ad278cf580297f870cdbc8a2b790951d97
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-25 19:55:03 +0530

    Add files via upload

---
+++ listcomp.py
@@ -0,0 +1,5 @@
+nums = input("Enter numbers separated by space: ").split()
+even_nums = [int(n) for n in nums if int(n) % 2 == 0]
+print("Even numbers:", even_nums)
+
+#[expression for item in iterable if condition]
