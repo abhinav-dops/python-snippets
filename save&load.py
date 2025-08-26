4a86af460e942f1234cf20c4e16ee7332df82072
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-27 00:26:30 +0530

    Add files via upload

---
+++ save&load.py
@@ -0,0 +1,21 @@
+import json
+
+expenses =  [
+    {"amount": 100, "category": "Food", "note": "Lunch"},
+    {"amount": 200, "category": "Travel", "note": "Taxi"}
+]
+
+with open("expenses.json" , "w") as file:
+    json.dump(expenses,file)
+
+with open("expenses.json", "r") as file:
+    loaded_file = json.load(file)
+    print(loaded_file)
+
+print("Expeses loaded from Json: ")
+for e in loaded_file:
+    print(e)
+
+for expense in loaded_file:
+    print("Amount:", expense["amount"])
+    print("Category:", expense["category"])
