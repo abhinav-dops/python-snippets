9325ecba9f6c1a3780610a9b291d898f6bb5aa0d
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-09-02 19:51:06 +0530

    Add files via upload

---
+++ fhmini.py
@@ -0,0 +1,18 @@
+with open("log.txt", "w") as file:
+    file.write("Abhinav")
+    file.write("\nPython")
+    file.write("\n15")
+
+#readline and readlines
+
+#readline: One line at a time.
+with open("log.txt", "r") as file:
+    line1 = file.readline()
+    line2 = file.readline()
+    print("Line 1:", line1)
+    print("Line 2:", line2)
+
+#readlines: read all lines into a list.
+with open("log.txt", "r") as file:
+    lines = file.readlines()
+    print(lines)
