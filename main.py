29335fb6fe4e87213ec4eb7aa1be2bfdf1b105c6
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-10-29 22:25:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from mypackage import math_utils, text_utils
+
+print("Add: ", math_utils.add(5, 3))
+print("Multiply: ", math_utils.multiply(4, 2))
+
+print(text_utils.shout("hello"))
+print(text_utils.whisper("HELLO"))
